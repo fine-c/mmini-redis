edf69dd36055dc615129c3ca5dc43d08df522b57
Cyan <2802660864@qq.com>
2025-05-16 10:43:48 +0800

    I/O

---
+++ src/bin/echo-server.rs
@@ -0,0 +1,33 @@
+use tokio::{
+    io::{self, AsyncReadExt, AsyncWriteExt},
+    net::TcpListener,
+};
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:6142").await.unwrap();
+
+    loop {
+        let (mut socket, _) = listener.accept().await?;
+
+        println!("new client.");
+        tokio::spawn(async move {
+            // 避免使用栈缓冲区 'let mut buf = [0, 1024];' 应使用堆缓冲区
+            let mut buf = vec![0; 1024];
+
+            loop {
+                match socket.read(&mut buf).await {
+                    Ok(0) => return,
+                    Ok(n) => {
+                        if socket.write_all(&buf[..n]).await.is_err() {
+                            return;
+                        }
+                    }
+                    Err(_) => {
+                        return;
+                    }
+                }
+            }
+        });
+    }
+}
